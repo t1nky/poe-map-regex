36adaf47b9f80f69d2ca481b01bcb8571ebecdba
alex <haiiryy@gmail.com>
2024-01-06 04:45:32 -0300

    github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://t1nky.github.io',
+  base: '/poe-map-regex',
   integrations: [tailwind()]
 });
