ccf51d2181180795556fda94b4cc7592a68deddd
alex <haiiryy@gmail.com>
2024-04-17 04:43:14 -0300

    t17 regex, alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://t1nky.github.io',
   base: '/poe-map-regex',
-  integrations: [tailwind()]
+  integrations: [tailwind(), alpinejs()]
 });
